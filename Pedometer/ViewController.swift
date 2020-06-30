1318dc3c273803d243b7fbf52015974d470cd381
mithunraj <mithun.raj159@gmail.com>
2020-06-30 16:35:44 -0700

    Initial Commit

---
+++ Pedometer/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Pedometer
+//
+//  Created by Mithun Raj on 30/06/20.
+//  Copyright © 2020 Mithun Raj. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
